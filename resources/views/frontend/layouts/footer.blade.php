e02cf6bd68bbce9d6f7d34077002c5dc0f4e6f8b
truonggdv <70989703+truonggdv@users.noreply.github.com>
2020-10-05 17:01:12 +0700

    update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -67,3 +67,28 @@
         </div>
     </div>
 </footer>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="100368618461351"
+  logged_in_greeting="Chào bạn! Chúng tôi giúp gì được cho bạn?"
+  logged_out_greeting="Chào bạn! Chúng tôi giúp gì được cho bạn?">
+      </div>
